023460de87a978b114ac6a8a6de63d6da9a18933
StylishCS <118624912+StylishCS@users.noreply.github.com>
2024-02-04 14:20:45 +0200

    test upload image

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const upload = require("../utils/uploadImage");
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.post("/test", upload.single("image"), (req, res) => {
+  return res
+    .status(200)
+    .send("https://ieee-backend-06597876c603.herokuapp.com/" + req.file.filename);
 });
 
 module.exports = router;
