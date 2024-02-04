2c1dc0c8e50741cd4f28086d8d4a13b9f936c029
StylishCS <118624912+StylishCS@users.noreply.github.com>
2024-02-04 15:01:48 +0200

    cloudinary init

--- routes/users.js
+++ routes/users.js
@@ -10,11 +10,23 @@ router.get("/", function (req, res, next) {
 
 router.post("/test", upload.single("image"), (req, res) => {
   try {
+    if (req.file) {
+      let streamUpload = (req) => {
+        return new Promise((resolve, reject) => {
+          let stream = cloudinary.uploader.upload_stream((error, result) => {
+            if (result) {
+              resolve(result);
+            } else {
+              reject(error);
+            }
+          });
+          streamifier.createReadStream(req.file.buffer).pipe(stream);
+        });
+      };
+      let result = await streamUpload(req);
     return res
       .status(200)
-      .send(
-        "https://ieee-backend-06597876c603.herokuapp.com/" + req.file.filename
-      );
+      .json(result)
   } catch (error) {
     return res.status(500).send(error);
   }
