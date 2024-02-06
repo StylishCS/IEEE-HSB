8eb7e4bf902daee72a5534d57c0b31c2a4ac0be5
StylishCS <118624912+StylishCS@users.noreply.github.com>
2024-02-06 03:09:04 +0200

    logging issues

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -4,24 +4,29 @@ const { Team } = require("../models/Team");
 async function AdminPrivileges(req, res, next) {
   try {
     if (!req.header("Authorization")) {
+      console.log("flag1")
       return res.status(401).json("FORBIDDEN");
     }
     const key = req.header("Authorization").split(" ")[0];
     const token = req.header("Authorization").split(" ")[1];
     if (key !== process.env.JWT_KEYWORD) {
+      console.log("flag2");
       return res.status(401).json("FORBIDDEN");
     }
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const admin = await Team.findById(decoded._id);
     if (!admin) {
+      console.log("flag3");
       return res.status(401).json("FORBIDDEN");
     }
     if (admin.role !== "Chairman") {
+      console.log("flag4");
       return res.status(401).json("FORBIDDEN");
     }
     req.adminId = decoded._id;
     next();
   } catch (error) {
+    console.log("flag5");
     return res.status(401).json("FORBIDDEN");
   }
 }
